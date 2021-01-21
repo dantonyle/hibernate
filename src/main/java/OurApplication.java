0b2b9e067bf68fd3b9b7babfd34a238164b4ef77
Dan Tony Le <dantonyle@gmail.com>
2021-01-21 08:52:23 -0600

    working and close final

--- src/main/java/OurApplication.java
+++ src/main/java/OurApplication.java
@@ -19,6 +19,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -32,52 +33,20 @@ public class OurApplication {
                     .openSession();
             hibernateSession.beginTransaction();
 
-            /* Insert some robots
-            for (int i = 0; i <= 10; i++) {
-                RobotEntity robot = new RobotEntity();
-                robot.setName("awesome robot number:" + i);
-                robot.setSwitchedOn((i % 2 == 0));
-                robot.setEvil((i % 2 == 1));
-                robot.setWeight(i * 100L);
-                robot.getParts().put("motor", "electric");
-                robot.getParts().put("power", "nuclear");
-                robot.getParts().put("weapon", "hammer");
-                hibernateSession.save(robot);
-            }
-             */
 
             /* Insert some laptops */
+            /*
             for (int i = 0; i <= 10; i++) {
                 LaptopEntity laptop = new LaptopEntity();
-                laptop.setName("SomeBook" + i);
-                laptop.setPrice(10F * i);
+                laptop.setName("SomeBook Series " + (i + 1));
+                laptop.setPrice((100F * i) + 500F);
                 hibernateSession.save(laptop);
             }
-
-            /* Get all robots
-            CriteriaBuilder builder = hibernateSession.getCriteriaBuilder();
-            CriteriaQuery<RobotEntity> criteria = builder.createQuery(RobotEntity.class);
-            criteria.from(RobotEntity.class);
-            List<RobotEntity> robots = hibernateSession.createQuery(criteria).getResultList();
-
-            robots.forEach((r) ->  {
-                System.out.println(
-                    "r: " + r.getId() +
-                            " name:" + r.getName() +
-                            " weight:" + r.getWeight() +
-                            " on:" + r.getSwitchedOn() +
-                            " evil:" + r.getEvil()
-                );
-                System.out.println("motor:" + r.getParts().get("motor"));
-                System.out.println("power:" + r.getParts().get("power"));
-                System.out.println("weapon:" + r.getParts().get("weapon"));
-                System.out.println("====================");
-            });
             */
 
 
             hibernateSession.getTransaction().commit();
-        } catch(Exception sqlException) {
+        } catch (Exception sqlException) {
             if (null != hibernateSession.getTransaction()) {
                 hibernateSession.getTransaction().rollback();
             }
--- src/main/java/widgetstore/web/LaptopDetailsServlet.java
+++ src/main/java/widgetstore/web/LaptopDetailsServlet.java
@@ -5,18 +5,23 @@ import ecommerce.LaptopEntity;
 import hibernate.HibernateUtils;
 import org.hibernate.Session;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 public class LaptopDetailsServlet extends HttpServlet {
 
     GenericDAO<DrinkDTO> drinkDAO;
 
     Session session;
+
+    Long lastID;
 
     public LaptopDetailsServlet() {
         drinkDAO = new DrinkDAOImpl();
@@ -29,27 +34,80 @@ public class LaptopDetailsServlet extends HttpServlet {
     public void doGet(HttpServletRequest request,
                       HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
+
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<LaptopEntity> criteria = builder.createQuery(LaptopEntity.class);
+        criteria.from(LaptopEntity.class);
+        List<LaptopEntity> laptops = session.createQuery(criteria).getResultList();
+
         PrintWriter out = response.getWriter();
         out.println("<form action='' method='POST'>");
-        out.println("<label>Enter Product (Laptop) ID: <input type='text' name='laptop-id'></input></label>");
+
+        out.println("<label>Enter Product ID of laptop below for pricing: <input type='text' name='laptop-id'></input></label>");
         out.println("<input type='submit'>Get Details</input>");
+        out.println("<br>");
+
+        /*
+        <style>
+                table, th, td {
+            border: 1px solid black;
+            border-collapse: collapse;
+            text-align: center;
+        }
+        </style>
+        */
+        out.println("<style>table, th, td { border: 1px solid black; border-collapse: collapse; text-align: center; } </style>");
+        out.println("<body>");
+        out.println("Available Laptops: ");
+        out.println("<table>");
+        out.println("<tr><th>Laptop_ID</th><th>Laptop_Name</th></tr>");
+        for (LaptopEntity laptop : laptops) {
+            //out.println("<br>");
+
+            //ut.println("ID: " + laptop.getId() + "- Name: " + laptop.getName() );
+            out.println("<tr><td>" + laptop.getId() + "</td><td>" + laptop.getName() + "</td></tr>");
+
+        }
+        out.println("</table>");
+        out.println("</body>");
         out.println("</form>");
     }
 
     public void doPost(HttpServletRequest request,
-          HttpServletResponse response) throws ServletException, IOException {
+                       HttpServletResponse response) throws ServletException, IOException, NumberFormatException {
+
+        response.setContentType("text/html");
         String laptopId = request.getParameter("laptop-id");
         PrintWriter out = response.getWriter();
 
-        LaptopEntity laptopEntity = session.get(
-                LaptopEntity.class,
-                Long.parseLong(laptopId)
-        );
-        if (laptopEntity != null) {
-            out.println("Found laptop: " + laptopEntity.getName() + " with price: " + laptopEntity.getPrice());
-        } else {
-            out.println("No laptop found for id: " + laptopId);
+        try {
+            LaptopEntity laptopEntity = session.get(
+                    LaptopEntity.class,
+                    Long.parseLong(laptopId)
+            );
+
+
+            if (laptopEntity != null) {
+                out.println("<form action='' 'method=POST'>");
+                out.println("<style>table, th, td { border: 1px solid black; border-collapse: collapse; text-align: center; } </style>");
+                out.println("<body>");
+                out.println("Product ID entered: " + laptopId);
+                out.println("<table>");
+                out.println("<tr><th>Laptop_ID</th><th>Laptop_Name</th><th>Laptop_Price</th></tr>");
+                out.println("<tr><td>" + laptopId + "</td><td>" + laptopEntity.getName() + "</td><td>" + laptopEntity.getPrice() + "</td></tr>");
+                //out.println("The laptop ID# " + laptopId + "- " + laptopEntity.getName() + " is priced at: " + laptopEntity.getPrice());
+                out.println("</table>");
+                out.println("</body>");
+                out.println("</form>");
+
+            } else {
+                out.println("No laptop found for id: " + laptopId);
+            }
+
+        }catch (NumberFormatException e) {
+            response.sendRedirect("laptop");
         }
+
     }
 
 }
